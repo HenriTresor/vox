b9a9831320ea5b7f64f2efb1c834501d9b70c902
henritresor <shimwamanahenritresor@gmail.com>
2023-08-31 20:44:51 +0200

    added a channel model

---
+++ server/models/Channel.model.js
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose'
+
+const ChannelSchema = new Schema({
+    name: { type: String, required: true, trim: true },
+    members: [{
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }],
+    form: {
+        type: String,
+        required: true,
+        enum: ['private', 'public'],
+        default: 'public'
+    },
+    creator: { type: Schema.Types.ObjectId, required: true, trim: true },
+    messages: [
+        {
+            type: String,
+            sender: { type: Schema.Types.ObjectId, ref: 'users' },
+            receiver: { type: Schema.Types.ObjectId, ref: 'users' },
+            message: { type: String },
+            sendOn: { type: Date, default: Date.now() }
+        }
+    ]
+},
+    {
+        timestamps: true,
+
+    }
+)
+
+export default model('channels', ChannelSchema)
